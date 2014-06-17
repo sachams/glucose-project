99564d708b9fcb1240f853bce88c217fa908680a
Sacha Manson-Smith <smansonsmith@gmail.com>
2014-06-17 22:54:01 +0100

    strava.sync.activities added

--- R/strava.R
+++ R/strava.R
@@ -1,6 +1,7 @@
 library(httr)
 library(jsonlite)
 library(httpuv)
+library(data.table)
 
 
 
@@ -19,7 +20,23 @@ strava.get.athlete <- function(strava.token) {
 
 strava.get.activities  <- function(strava.token) {
 	## TODO - add pagination
-	req <- GET('https://www.strava.com/api/v3/athlete/activities', 
+	all.activities <- NULL
+	page <- 1
+	
+	repeat {
+		one.page <- strava.get.activitypage(strava.token, page)
+		if(is.null(one.page)) {
+			break
+		}
+		
+		## Er....how do you rbind the datasets? Get a wierd error...
+	}
+}
+
+strava.get.activitypage  <- function(strava.token, page) {
+	reqUrl <- paste('https://www.strava.com/api/v3/athlete/activities','?per_page=100&page=',page,sep='')
+	message(reqUrl)
+	req <- GET(reqUrl, 
 			   config(token = strava.token))
 	stop_for_status(req)
 	reformat.json(req)
@@ -36,12 +53,71 @@ strava.get.activity  <- function(strava.token, activity.id) {
 	reformat.json(req)
 }
 
-strava.download.streams <- function(strava.token, directory, activity.id, stream) {
-	## Download the streams to a CSV file in the specified directory
-	df <- strava.get.streams(strava.token)
+strava.sync.activities <- function(strava.token, directory, skip.existing=TRUE, single.id=NULL) {
+	## Get a list of all the activities
+	activities <- strava.get.activities(strava.token)
+	
+	## Simplify the results
+	activities <- activities[,c('id','start_date_local', 'manual')]
+	
+	## See if a single activity has been specified
+	if(!is.null(single.id)) {
+		message(paste('Downloading only a single ID', single.id))
+		activities <- activities[activities$id==single.id,]
+	}
+	
+	## Apply the download function to each result
+	apply(activities, 1, function(x) { 
+		start.time <- as.POSIXct(strptime(x$start_date_local,'%Y-%m-%dT%H:%M:%SZ'))
+		
+		## Manual entries don't have any streams, so skip
+		is.manual <- x$manual
+		if(is.manual) {
+			message(paste('Skipping activity',x$id,'as it was manually entered'))
+		} else {
+			strava.download.streams(strava.token, 
+									directory, 
+									x$id, 
+									c('time', 'heartrate'), 
+									skip.existing=skip.existing, 
+									start.time=start.time)
+		}
+	} )
 }
 
-strava.get.streams <- function(strava.token, activity.id, stream) {
+
+strava.download.streams <- function(strava.token, 
+									directory, 
+									activity.id, 
+									streams, 
+									skip.existing=TRUE, 
+									start.time=NULL) {
+	## Build filename in the form activityid.csv
+	filename <- sprintf("%s/%d.csv", directory, activity.id)
+
+	## Check if the file exists alrady. If it does, skip.
+	if(file.exists(filename) && skip.existing) {
+		message(paste("Not downloading activity", activity.id,"as target file already exists"))
+	} else {
+		
+		## Download the streams to a CSV file in the specified directory
+		if(file.exists(filename)) {
+			message(paste("Re-downloading activity", activity.id,"..."))
+		} else {
+			message(paste("Downloading activity", activity.id,"..."))
+		}
+		dt <- data.table(strava.get.streams(strava.token, activity.id, streams))
+	
+		## If a start date is specified, then add in an additional column with the absolute time
+		if(!is.null(start.time)) {
+			dt[,abs.time:=time+start.time]
+		}	
+
+		write.csv(dt, filename, row.names=FALSE)
+	}
+}
+
+strava.get.streams <- function(strava.token, activity.id, streams) {
 	## Check stream type is allowed
 
 	
