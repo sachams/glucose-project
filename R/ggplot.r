9099aa9bf9b56c310618794e1d66c4fe1485f8ed
sachams <sachams@users.noreply.github.com>
2014-06-20 15:10:08 +0100

    Rename R/ggplot.r to R/misc/ggplot.r

--- R/ggplot.r
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-library(scales)
-
-ggplot() + 
-geom_step(data=data, aes(x=date, y=total.basal)) +
-geom_bar(stat="identity", data=data, aes(x=date, y=trimp.exp)) + 
-xlab("Date") + ylab("Total Basal (units)") + 
-scale_y_continuous(limits=c(12,24), breaks=seq(12,24,2)) + scale_x_date(limits=c(as.Date("2014-01-01"), as.Date("2014-05-01")), breaks=seq(as.Date("2014-01-01"), as.Date("2014-05-01"), by="2 week"), labels=date_format("%d-%b"))
